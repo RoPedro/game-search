3387bfcd80d0139e855427da88e9291b448f50ef
Pedro Moreira <pedro.lucas.rocha72@gmail.com>
2026-05-11 19:28:42 -0300

    refactor: migrates commands to igdb API

--- commands.py
+++ commands.py
@@ -2,45 +2,41 @@ import requests
 from os import getenv
 from dotenv import load_dotenv
 import nextcord
+import json
 
 from core.utils import convert_date
+from core.igdb_auth import wrapper
 from models.game import Game
 
 load_dotenv()
 
-API_KEY = getenv("API_KEY")
-BASE_URL = getenv("BASE_URL")
-
 
 def gsearch_command(query: str):
-    url_query = f"https://{BASE_URL}/v1/Games/ByGameName"
-    developer_query = f"https://{BASE_URL}/v1/Developers/ByDeveloperID"
-    thumb_base_url = "https://cdn.thegamesdb.net/images/thumb/"
-
-    base_response = requests.get(
-        url_query, params={"apikey": API_KEY, "name": query}
-    ).json()
+    game_response = wrapper.api_request(
+        "games",
+        f'fields name, involved_companies.company.name, first_release_date, cover.url; search "{query}"; where game_type = 0; limit 4;',
+    )
+    igdb_data = json.loads(game_response)
 
     game = [
         {
-            "game_id": base_response["data"]["games"][0]["id"],
-            "game_title": base_response["data"]["games"][0]["game_title"],
-            "developers": base_response["data"]["games"][0]["developers"],
-            "release_date": base_response["data"]["games"][0]["release_date"],
+            "game_id": igdb_data[0]["id"],
+            "game_title": igdb_data[0]["name"],
+            "developer": igdb_data[0]["involved_companies"][0]["company"]["name"],
+            "release_date": igdb_data[0][
+                "first_release_date"
+            ],  # Unix timestamp, needs conversion
+            "small_thumb": igdb_data[0]["cover"]["url"].replace(
+                "t_thumb", "t_cover_big"
+            ),
         }
     ]
 
-    dev_id = str(game[0]["developers"][0])
-    developer_response = requests.get(
-        developer_query, params={"apikey": API_KEY, "id": dev_id}
-    ).json()
-    dev_name = developer_response["data"]["developers"][dev_id]["name"]
-
     gameObject = Game(
         title=game[0]["game_title"],
-        small_thumb=f"{thumb_base_url}boxart/front/{game[0]['game_id']}-1.jpg",
-        developer=dev_name,
+        developer=game[0]["developer"],
         release_date=game[0]["release_date"],
+        small_thumb="https:" + game[0]["small_thumb"],
     )
 
     # Assembles the final embed
@@ -49,11 +45,11 @@ def gsearch_command(query: str):
         description=f"Developer: {gameObject.get_developer()}\nRelease Date: {convert_date(gameObject.get_release_date())}",
     )
     embed.set_image(url=gameObject.get_small_thumb())
-    embed.set_footer(text="Data provided by TheGamesDB")
+    embed.set_footer(text="Data provided by IGDB")
 
     print(f"Title: {gameObject.get_title()}")
     print(f"Small Thumb: {gameObject.get_small_thumb()}")
     print(f"Developer: {gameObject.get_developer()}")
     print(f"Release Date: {convert_date(gameObject.get_release_date())}")
-    
+
     return embed
