e6546fd3cef18a2d6ed122dbf2b7cd28acda7595
Pedro Moreira <pedro.lucas.rocha72@gmail.com>
2026-05-11 19:25:49 -0300

    feat: add igdb wrapper

---
+++ core/igdb_auth.py
@@ -0,0 +1,10 @@
+from igdb.wrapper import IGDBWrapper
+from dotenv import load_dotenv
+from os import getenv
+
+load_dotenv()
+
+IGDB_CLIENT_ID = getenv('IGDB_CLIENT_ID')
+IGDB_TOKEN = getenv('IGDB_TOKEN')
+
+wrapper = IGDBWrapper(str(IGDB_CLIENT_ID), str(IGDB_TOKEN))
