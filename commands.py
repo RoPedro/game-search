22e30862ce7b7445aeb4fe985bf53d8cdea6ff4d
Pedro Moreira <pedro.lucas.rocha72@gmail.com>
2026-05-13 11:33:53 -0300

    refactor: move array population and embed building to separate files
    games array population now happens in utils.py/create_games_array
    embed building now happens in utils.py/build_embed

--- commands.py
+++ commands.py
@@ -1,55 +1,20 @@
-import requests
-from os import getenv
 from dotenv import load_dotenv
-import nextcord
 import json
 
-from core.utils import convert_date
+from core.utils import build_embed, create_games_array
 from core.igdb_auth import wrapper
-from models.game import Game
 
 load_dotenv()
 
 
 def gsearch_command(query: str):
+    LIMIT = 4
     game_response = wrapper.api_request(
         "games",
-        f'fields name, involved_companies.company.name, first_release_date, cover.url; search "{query}"; where game_type = 0; limit 4;',
+        f'fields name, involved_companies.company.name, first_release_date, cover.url; search "{query}"; where game_type = 0; limit {LIMIT};',
     )
     igdb_data = json.loads(game_response)
 
-    game = [
-        {
-            "game_id": igdb_data[0]["id"],
-            "game_title": igdb_data[0]["name"],
-            "developer": igdb_data[0]["involved_companies"][0]["company"]["name"],
-            "release_date": igdb_data[0][
-                "first_release_date"
-            ],  # Unix timestamp, needs conversion
-            "small_thumb": igdb_data[0]["cover"]["url"].replace(
-                "t_thumb", "t_cover_big"
-            ),
-        }
-    ]
-
-    gameObject = Game(
-        title=game[0]["game_title"],
-        developer=game[0]["developer"],
-        release_date=game[0]["release_date"],
-        small_thumb="https:" + game[0]["small_thumb"],
-    )
-
-    # Assembles the final embed
-    embed = nextcord.Embed(
-        title=gameObject.get_title(),
-        description=f"Developer: {gameObject.get_developer()}\nRelease Date: {convert_date(gameObject.get_release_date())}",
-    )
-    embed.set_image(url=gameObject.get_small_thumb())
-    embed.set_footer(text="Data provided by IGDB")
-
-    print(f"Title: {gameObject.get_title()}")
-    print(f"Small Thumb: {gameObject.get_small_thumb()}")
-    print(f"Developer: {gameObject.get_developer()}")
-    print(f"Release Date: {convert_date(gameObject.get_release_date())}")
-
+    games = create_games_array(igdb_data, limit=LIMIT)
+    embed = build_embed(games)
     return embed
--- core/utils.py
+++ core/utils.py
@@ -1,4 +1,6 @@
 from datetime import datetime
+from models.game import Game
+from nextcord import Embed
 
 
 # Don't crash if date is invalid
@@ -9,3 +11,36 @@ def convert_date(date_stamp):
         return datetime.fromtimestamp(date_stamp).strftime("%d/%m/%Y")
     except ValueError:
         return None
+
+
+def create_games_array(igdb_data, limit=4):
+    games = []
+    for i in range(limit):
+        try:
+            game = Game( # iterate with i because igdb games list don't have a "game" key, so number instead.
+                title=igdb_data[i]["name"],
+                developer=igdb_data[i]["involved_companies"][0]["company"]["name"],
+                release_date=igdb_data[i]["first_release_date"],
+                small_thumb="https:" + igdb_data[i]["cover"]["url"].replace(
+                    "t_thumb", "t_cover_big"
+                ),
+            )
+            games.append(game)
+        except (IndexError, KeyError):
+            continue
+    return games
+
+def build_embed(games):
+    embed = Embed(
+        title=games[0].get_title(),
+        description=f"Developer: {games[0].get_developer()}\nRelease Date: {convert_date(games[0].get_release_date())}",
+    )
+    embed.set_image(url=games[0].get_small_thumb())
+    embed.set_footer(text="Data provided by IGDB")
+
+    print(f"Title: {games[0].get_title()}")
+    print(f"Small Thumb: {games[0].get_small_thumb()}")
+    print(f"Developer: {games[0].get_developer()}")
+    print(f"Release Date: {convert_date(games[0].get_release_date())}")
+
+    return embed
