ac3f05337dbe6cd9e71f7084cd06b170c19170d1
Pedro Moreira <pedro.lucas.rocha72@gmail.com>
2026-05-16 21:15:00 -0300

    feat: add publishers
    additionally, developer is now the actual dev team, not the hardcoded
    first one.

--- core/utils.py
+++ core/utils.py
@@ -17,23 +17,38 @@ def create_games_array(igdb_data, limit=4):
     games = []
     for i in range(limit):
         try:
-            game = Game( # iterate with i because igdb games list don't have a "game" key, so number instead.
+            data_developer, data_publisher = find_set_companies(
+                igdb_data[i]
+            )  # send the index so it knows what game is. (data[game_index])
+            game = Game(  # iterate with i because igdb games list don't have a "game" key, so number instead.
                 title=igdb_data[i]["name"],
-                developer=igdb_data[i]["involved_companies"][0]["company"]["name"],
+                developer=data_developer,
+                publisher=data_publisher,
                 release_date=igdb_data[i]["first_release_date"],
-                small_thumb="https:" + igdb_data[i]["cover"]["url"].replace(
-                    "t_thumb", "t_cover_big"
-                ),
+                small_thumb="https:"
+                + igdb_data[i]["cover"]["url"].replace("t_thumb", "t_cover_big"),
             )
             games.append(game)
         except (IndexError, KeyError):
             continue
     return games
 
+
+def find_set_companies(igdb_data):
+    developer, publisher = "", ""
+    for company in igdb_data["involved_companies"]:  # loop in involved companies
+        if company["developer"] == True:
+            developer = company["company"]["name"]
+        elif company["publisher"] == True:
+            publisher = company["company"]["name"]
+
+    return developer, publisher
+
+
 def build_embed(games):
     embed = Embed(
         title=games[0].get_title(),
-        description=f"Developer: {games[0].get_developer()}\nRelease Date: {convert_date(games[0].get_release_date())}",
+        description=f"Developer: {games[0].get_developer()}\nPublisher: {games[0].get_publisher()}\nRelease Date: {convert_date(games[0].get_release_date())}",
     )
     embed.set_image(url=games[0].get_small_thumb())
     embed.set_footer(text="Data provided by IGDB")
@@ -41,6 +56,7 @@ def build_embed(games):
     print(f"Title: {games[0].get_title()}")
     print(f"Small Thumb: {games[0].get_small_thumb()}")
     print(f"Developer: {games[0].get_developer()}")
+    print(f"Publisher: {games[0].get_publisher()}")
     print(f"Release Date: {convert_date(games[0].get_release_date())}")
 
     return embed
