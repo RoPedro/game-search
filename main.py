25b4f3c558a158d21c1562c295ed3ac66d5d4e0d
Pedro Moreira <pedro.lucas.rocha72@gmail.com>
2026-05-21 12:20:36 -0300

    feat: gsearch command now has a cooldown
    it's using lambda instead of BucketType because Nextcord cooldowns is
    partially broken.

--- main.py
+++ main.py
@@ -31,22 +31,39 @@ elif ENV == "development":
 
 bot = commands.Bot(command_prefix=prefix, intents=intents)
 
-
 @bot.command(name="gs", description="Search for a game by its name")
+@commands.cooldown(1, 5, lambda message: message.author.id)
 async def gsearch(ctx, *, query: str):
+    """
+    According to Nextcord issue [#1282](https://github.com/nextcord/nextcord/issues/1282), cooldowns for non-slash commands are broken.
+    Tipically we would use @commands.cooldown(x, y, BucketType.user), which returns message.author.id. Since not working, we use lambda instead.
+    Lambda works because cooldown() only blocks BucketType, so we bypass by getting the author ID directly from the message object.
+    """
     from commands import gsearch_command
 
     embed = gsearch_command(query)
-    
+
     if embed is not None:
         await ctx.send(embed=embed)
     else:
         no_results = nextcord.Embed(
-        title="No Games Found",
-        description="No games matched your search. Try a different title or check your spelling.",
-        color=nextcord.Color.red()
-    )
+            title="No Games Found",
+            description="No games matched your search. Try a different title or check your spelling.",
+            color=nextcord.Color.red(),
+        )
         await ctx.send(embed=no_results)
+
+
+@bot.event
+async def on_command_error(ctx, error):
+    error = getattr(error, "original", error)
+    if isinstance(error, commands.CommandOnCooldown):
+        cd_embed = nextcord.Embed(
+            title="🤚 Command on Cooldown 🤚",
+            description=f"Please wait {error.retry_after:.1f} seconds before using this command again.",
+            color=nextcord.Color.orange(),
+        )
+        await ctx.send(embed=cd_embed)
 
 
 bot.run(str(TOKEN))
